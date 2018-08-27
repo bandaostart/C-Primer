206d71f81fbe7219bf9278c709594936cc08be3e
bandaostart <bingranyixiao@163.com>
2018-08-27 17:31:26 +0800

    2018-08-27

--- 12-1-Smart_Point/main.cpp
+++ 12-1-Smart_Point/main.cpp
@@ -24,6 +24,22 @@ shared_ptr：独有的操作
     程序不知道自己需要使用多少对象   容器类的使用时出于这个原因
     程序不知道所需对象的准确类型
     程序需要在多个对象间共享数据
+4，unique_ptr操作函数：
+    unique_ptr<T> u1:      空unique_ptr可以指向类型为T的对象，u1会使用delete
+    u = nullptr     :      释放u指向的对象，将u置为空
+    u.release()     ：      u放弃对指针的控制权，返回指针，并将u置为空; 并不会释放指针
+    u.reset()       ：      释放u指向的对象
+    u.reset(q)      :      如果提供了内置指针q，令u指向这个对象；否则将u置为空
+    u.reset(nullptr)：
+5，new T[]          :      动态分配数组
+
+6,allocator类：
+    allocator<T> a         :      定义一个名为a的allocator对象，它可以为类型为T的对象分配内存
+    a.allocate(n)          :      分配一段原始的，未构造的内存，保存n个类型为T的对象
+    a.deallocate(p,n)      :      释放从T*指针p中地址开始的内存
+    a.construct(p, args)   :      p必须是一个类型为T*的指针，指向一块原始内存，arg被传递给类型weiT的构造函数
+    a.destroy(p)           :      p为T*类型的指针，此算法对p指向的对象执行西沟函数
+
 */
 
 using namespace std;
@@ -58,32 +74,65 @@ int main()
     auto q(p7);                                      //p7和q指向相同对象，此对象有两个引用
 
 
+    /*unique_ptr指向它所指的对象，当它被销毁时它所指的对象也被销毁----------------------------------*/
+    //当我们定义unique_ptr时，需要将其绑定到new返回的指针上，unique_ptr不支持普通的拷贝和赋值操作
+    unique_ptr<int> p8(new int(42));                 //p8指向一个值为42的int
+    p8 = nullptr;                                    //释放指针，将p8置为空
+
+    unique_ptr<string> p9(new string("hello point"));
+    unique_ptr<string> p10(p9.release());            //将p9的所有权转移给p10
+    unique_ptr<string> p11(new string("Text"));
+    p10.reset(p11.release());                        //将所有权从p11转到p10；reset释放了p10原来指向的内存
+
+    /*new T[]动态数组----------------------------------------------------------------------*/
+    int *pa      = new int;
+    int *pia     = new int[10];                          //10个未初始化的int
+    int *pia2    = new int[10]();                        //10个值初始化为0的int
+    string *psa  = new string[10];                       //10个空string
+    string *psa2 = new string[10]();                     //10个空string
+
+    int    *pia3 = new int[10]{0,1,2,3,4,5,6,7,8,9};     //10个int分别进行初始化
+    string *psa3 = new string[10]{"a", "an", "the", string(3, 'x')}; //前4个进行初始化
+
+    delete pa;                                          //释放指针动态分配的对象
+
+    delete [] pia;                                       //释放动态分配的数组
+    delete [] pia2;
+    delete [] pia3;
+
+    delete [] psa;
+    delete [] psa2;
+    delete [] psa3;
+
+    /*unique_ptr分配数组------------------------------------------------------------------*/
+    unique_ptr<int[]> up(new int[10]);                      //up指向一个包含10未初始化int的数组
+    for (size_t i=0; i<10; i++)
+    {
+        up[i] = i;
+    }
+    up.release();                                           //自动用delete[]销毁其指针
 
 
+    /*shared_ptr管理动态数组，shared_ptr不直接支持管理动态数组，如果希望使用shared_ptr管理，必须提供自
+     * 己定义的删除器----------------------------------------------------------------------*/
+    shared_ptr<int> sp(new int[10], [] (int *p) {delete[] p;});
+    sp.reset();                                             //使用我们提供的lambda释放数组，他是用detete[]
 
 
+    /*allocatro使用---------------------------------------------------------------------*/
+    allocator<string> alloc;                                //可以分配string的allocator对象
+    auto const p = alloc.allocate(10);                      //分配n个未初始化的string
 
+    auto m = p;                                             //q指向最后构造的元素之后的位置
+    alloc.construct(m++);                                   //*q为空字符串
+    alloc.construct(m++, 10, 'c');                          //*q为cccccccc
+    alloc.construct(m++, "hi");                             //*q为hi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while (m != p)
+    {
+        cout << *(--m) << endl;
+        alloc.destroy(m);                                  //释放真正构造的string
+    }
 
 
     return 0;
---
+++ 13-1-ClassDesigner/main.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <memory>
+
+/*
+1,拷贝构造函数，拷贝赋值运算符，移动构造函数，移动赋值运算符，析构函数
+  拷贝和移动构造函数定义了当用同类型的另一个对象初始化本对象时做什么
+  拷贝和移动赋值运算符定义了将一个对象赋予同类型的另一个对象时做什么
+  析构函数定义了将一个对象销毁时做什么
+
+
+
+*/
+using namespace std;
+class Sales_data
+{
+    public:
+       ~Sales_data();                                                 //析构函数
+    private:
+};
+
+Sales_data::~Sales_data()
+{
+
+}
+
+
+
+class Test_Num1
+{
+    public:
+        Test_Num1(void)                                                //构造函数
+        {
+            x = 0;
+            y = 0;
+            cout << "Tset_Num1 Constructor be called" <<endl;
+        }
+
+        ~Test_Num1()                                                  //析构函数
+        {
+
+        }
+
+        Test_Num1(const Test_Num1 &tc)                                //拷贝构造函数，必须使用引用
+        {
+            x=tc.x;
+            y=tc.y;
+            cout << "Test_Num1 Copy Constructor Called" << endl;
+        }
+
+        const Test_Num1 &operator = (const Test_Num1 &right)          //赋值构造函数
+        {
+            x = right.x;
+            y = right.y;
+            cout << "Tset_Num1 Operator = be Called" << endl;
+            return *this;
+        }
+    private:
+        int x, y;
+};
+
+class Test_Num2
+{
+    public:
+        Test_Num2()                                                      //构造函数
+        {
+            cout << "Test_Num2 Constructor be called" <<endl;
+        }
+
+        explicit Test_Num2(const Test_Num1 &tcc)                         //显示构造函数
+        {
+            tc = tcc;
+        }
+
+        ~Test_Num2()                                                    //析构函数
+        {
+
+        }
+
+        Test_Num2(const Test_Num2 &test_num2): tc(test_num2.tc)        //拷贝构造函数
+        {
+            tc = test_num2.tc;
+            cout << "Test_Num2 Copy Constructor be called" << endl;
+        }
+
+        const Test_Num2 &operator = (const Test_Num2 &right)           //赋值构造函数
+        {
+            tc = right.tc;
+            cout << "Test_Num2 Operator = be Called" << endl;
+
+            return *this;
+        }
+    private:
+        Test_Num1 tc;
+};
+
+
+
+
+
+
+int main(void)
+{
+    string dots(10, '.');                                               //直接初始化
+    string s(dots);                                                     //直接初始化
+    string ss = dots;                                                   //拷贝初始化
+    string null_book = "999999";                                        //拷贝初始化
+    string nines = string(100, '9');                                    //拷贝初始化
+
+
+    //退出局部作用域；对item p2 和vec调用析构函数
+    //销毁p2会递减其引用计数；如果引用计数变为0，对象被释放
+    //销毁vec会销毁它的元素
+    {
+        //p和p2指向动态分配的对象
+        Sales_data *p = new Sales_data;                                 //p是一个内置指针
+        auto p2 = make_shared<Sales_data>();                            //p2是一个shared_ptr
+        Sales_data item(*p);                                            //拷贝构造函数将*p拷贝到item中
+        vector<Sales_data>vec;                                          //局部对象
+        vec.push_back(*p2);                                             //拷贝p2指向的对象
+        delete p;
+    }
+
+    string s1("hello"), s2 = s1;                                        //拷贝构造函数
+    string s3("hello"), s4;
+    s3 = s4;                                                            //赋值运算重载
+
+    //如果一个类，需要析构函数，我们几乎可以肯定它也需要一个拷贝构造函数和一个拷贝赋值运算符
+    cout << "Test_Num1 test_num1" << endl;
+    Test_Num1 test_num1;
+    cout << endl;
+
+    cout << "Test_Num1 test_num2 = test_num1" << endl;
+    Test_Num1 test_num2 = test_num1;
+    cout << endl;
+
+    cout << "Test_Num1 test_num3(test_num2)" << endl;
+    Test_Num1 test_num3(test_num2);
+    cout << endl;
+
+    cout << "test_num3 = test_num1" << endl;
+    test_num3 = test_num1;
+    cout << endl;
+
+    cout << "Test_Num2 test1" << endl;
+    Test_Num2 test1;
+    cout << endl;
+
+    cout << "Test_Num2 test2=test1" << endl;
+    Test_Num2 test2=test1;
+    cout << endl;
+
+    cout << "Test_Num2 test3(test2)" << endl;
+    Test_Num2 test3(test2);
+    cout << endl;
+
+    cout << "test3 = test1" << endl;
+    test3 = test1;
+    cout << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
